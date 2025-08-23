1b2c1763ad3ced8659b7d75b453e5bfd4d1ee258
koo1029 <145544644+koo1029@users.noreply.github.com>
2025-08-23 22:36:02 +0900

    Equipment 자료 설정

--- Assets/Scripts/Equipment/Equipment.cs
+++ Assets/Scripts/Equipment/Equipment.cs
@@ -11,6 +11,23 @@ public enum EEquipmentType
 
 public class Equipment : MonoBehaviour
 {
+    [SerializeField]
+    private StatScriptable _stat;
+    
+    [SerializeField]
+    private int _equipmentLevel = 0;
+    
+    [SerializeField]
+    private EEquipmentType _equipmentType;
+    
+    [SerializeField]
+    private Sprite _equipmentIcon;
+    
+    public StatScriptable Stat => _stat;
+    public int EquipmentLevel => _equipmentLevel;
+    public EEquipmentType EquipmentType => _equipmentType;
+    public Sprite EquipmentIcon => _equipmentIcon;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
