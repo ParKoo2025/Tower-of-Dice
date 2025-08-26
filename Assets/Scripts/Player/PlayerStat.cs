e4ad62c95fa4db6ef132e7420511bac2352a42b3
koo1029 <145544644+koo1029@users.noreply.github.com>
2025-08-26 21:01:27 +0900

    장비 착용, 해체 스탯만 구현

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -5,35 +5,49 @@ using UnityEngine;
 public class PlayerStat : MonoBehaviour
 {
     [SerializeField] private StatScriptable _basicStat;
-    [SerializeField] private List<Equipment> _equipment = new List<Equipment>();
+    [Header("장비 슬롯 (타입별 1칸)")]
+    [SerializeField] private Equipment[] _equipped = new Equipment[(int)EEquipmentType.Size];
 
     public Stat TotalStat { get; private set; } = new Stat();
 
     public void SetEquipment(Equipment eq)
     {
-        //_equipmentStat[(int)equipmentType] = equipmentStat;
-        CalculateStat();
-    }    
+        var idx = (int)eq.EquipmentType;
+        if (_equipped[idx] != null) UnEquip(idx);
+        _equipped[idx] = eq;
+        AddEquipmentStat(idx);
+    }
+
+    private void UnEquip(int idx)
+    {
+        SubtractEquipmentStat(idx);
+        _equipped[idx] = null;
+    }
 
     private void Start()
     {
-        CalculateStat();
+        for (int i = 0; i < (int)EStatType.Size; i++)
+        {
+            TotalStat[(EStatType)i] = _basicStat[(EStatType)i];
+        }
     }
     
     /// <summary>
     /// TotalStat을 계산한다.
     /// </summary>
-    private void CalculateStat()
+    private void AddEquipmentStat(int idx)
     {
         for (int i = 0; i < (int)EStatType.Size; i++)
         {
-            float sum = _basicStat[(EStatType)i];
-            for (int j = 0; j < (int)EEquipmentType.Size; j++)
-            {
-               // sum += _equipment[j][(EStatType)i];
-            }
-
-            TotalStat[(EStatType)i] = sum;
+            TotalStat[(EStatType)i] += _equipped[idx].Stat[(EStatType)i];
+        }
+    }
+    
+    private void SubtractEquipmentStat(int idx)
+    {
+        for (int i = 0; i < (int)EStatType.Size; i++)
+        {
+            TotalStat[(EStatType)i] -= _equipped[idx].Stat[(EStatType)i];
         }
     }
 }
